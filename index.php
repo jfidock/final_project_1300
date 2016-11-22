48ead525bfe5fd22ce96544ddb38b94e34ba2a71
Jeremy Fidock <jeremyfidock@Jeremys-MBP.home>
2016-11-22 11:06:41 -0500

    Adding milestone2 files

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- 
+* INFO/CS 1300
+* Fall 2016
+* Jeremy Fidock
+* Final Project
+* Milestone 2
+ -->
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Apple Harvest Festival</title>
+    <link rel="stylesheet" type="text/css" href="styles/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/site.css">
+    <link rel="stylesheet" type="text/css" href="styles/bannerStyle.css">
+  </head>
+  <body>
+    <?php   
+      // call php file to read csv and update html with information
+      include 'scripts/banner_and_nav.php';
+    ?>
+    <!-- <div id="title">
+      <h1>Basic Archery Terms</h1>
+    </div>
+    
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="contact.html">Contact</a>
+    </nav> -->
+
+    <div id="container">
+      <!-- <h1>Glossary of Basic Archery Terms</h1> -->
+
+    </div>
+    <footer>
+    <!-- And that's the sound of sunshine... -->
+    </footer>
+  </body>
+</html>
---
+++ scripts/banner_and_nav.php
@@ -0,0 +1,29 @@
+<!-- 
+INFO/CS 1300
+Fall 2016
+Jeremy Fidock
+Final Project Milestone 2
+ -->
+
+ <!-- Banner and Nav Bar -->
+
+<div id="top">
+
+	<div id="banner">
+		<h1 id="overlay">34th Annual Apple Harvest Festival</h1>
+	</div>
+	<!-- End of banner div -->
+
+	<div id="navBar">
+		<span class="navSpan"><a href="#">Home</a></span>
+		<span class="navSpan"><a href="events.php">Events</a></span>
+		<span class="navSpan"><a href="entertainment.php">Entertainment</a></span>
+		<span class="navSpan"><a href="information.php">Information</a></span> 
+			<!-- times and transportation -->
+			<!-- take map off of ithaca page -->
+
+	</div>
+	<!-- End of navBar div -->
+
+</div>
+<!-- End of top div -->
