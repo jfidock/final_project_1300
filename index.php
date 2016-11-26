ba0cf5f3700a5ff4079d0f1381abf4341d9d62d3
Claire W <cdwang@bu.edu>
2016-11-26 12:00:02 -0500

    updated index.php and bannerstyle.css

--- index.php
+++ index.php
@@ -11,9 +11,10 @@
   <head>
     <meta charset="utf-8">
     <title>Apple Harvest Festival</title>
-    <link rel="stylesheet" type="text/css" href="styles/normalize.min.css">
-    <link rel="stylesheet" type="text/css" href="styles/site.css">
+    <!-- <link rel="stylesheet" type="text/css" href="styles/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/site.css"> -->
     <link rel="stylesheet" type="text/css" href="styles/bannerStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
   </head>
   <body>
     <?php   
