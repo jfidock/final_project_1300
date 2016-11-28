2eeeed31d3381620e3b748a9c98073f6cabf9455
Jeremy Fidock <jeremyfidock@dhcp-rhodes-425.eduroam.cornell.edu>
2016-11-28 17:15:06 -0500

    changes to index adding info

--- index.php
+++ index.php
@@ -17,13 +17,17 @@
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
   </head>
   <body>  
-  // call php file to read csv and update html with information
+  <!-- call php file to read csv and update html with information -->
   <?php include 'scripts/banner_and_nav.php';?>
 
     <div id="container">
       <div id="About">
         <h1>Apple Harvest History</h1>
-        <p></p>
+        <p>The 2016 Apple Harvest Festival hosts plenty of apples, local produce, fresh baked goods, family entertainment at every turn, games, rides, prizes and live performances on two stages. Apple Harvest Festival is a great introduction to the rich farm and artist community of Ithaca.  First time visitors can witness a variety of wonderful local entertainment while also visiting the historical outdoor pedestrian shopping mall, The Ithaca Commons.  The pedestrian mall is the epicenter of the festival and the gateway to a nostalgic journey to harvest season fun.
+
+        Over 100 vendors will be selling unique apple varieties, hot apple cider donuts, kettle corn, local farm fresh baked goods and a multitude of around the world specialties. Local farmers are on site selling everything from tasty apple beverages to local pumpkins and of course, apples, apples and more apples. Wineries and cideries have regional beverages available for tasting and purchase.  Kids can visit with the goats and learn how fiber is made with Laughing Goat Fiber Farm.  Looking for quality goods, visit 60 different handcrafting artisans along Cayuga Street at the fall craft show which includes jewelry, ceramics, home decor, body care, specialty gifts, unique sculpture, clothing and so much more.</p>
+        <!-- Information taken from the 34th Annual Harvest Festival page 
+        http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
       </div>
 
       <div id="Events">
